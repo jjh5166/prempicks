6f0ccc90419ba87708d95571b7b52219f725e206
John <jjh5166@gmail.com>
2018-07-31 00:28:43 -0400

    init

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,17 @@
+class ApplicationController < ActionController::Base
+  FootballData.configure do |config|
+    # get api key at 'http://api.football-data.org/register'
+    config.api_key = '34cddfbc8f4a4b67acafbdc8b5db300d'
+
+    # default api version is 'alpha' if not setted
+    config.api_version = 'v2'
+
+    # the default control method is 'full' if not setted
+    # see request section on 'http://api.football-data.org/documentation'
+    config.response_control = 'full'
+    # config.response_control = 'minified'
+  end
+
+  $epl_table = FootballData.fetch(:competitions, :matches, id: 2021)
+  # :leagueTable, id: 445
+end
