aed1ae1a2e1fab02e05c0121c1f556d29f8bdf4a
John <jjh5166@gmail.com>
2018-08-07 19:28:42 -0400

    required aws s3

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
-# require 'aws-sdk-s3'
+require 'aws-sdk-s3'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
