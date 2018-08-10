9e88a3dd4e0b26b564ca91b056da2ed1b56d5335
John <jjh5166@gmail.com>
2018-08-09 21:14:46 -0400

    picks validate, font, picks create

--- app/controllers/epldata_controller.rb
+++ app/controllers/epldata_controller.rb
@@ -4,15 +4,7 @@ class EpldataController < ApplicationController
   def table
   @epl_table = FootballData.fetch(:competitions,:standings, id: 2021)['standings'][0]['table']
 # MUST MOVE TO one time welcome page
-    if (user_signed_in? && current_user.sign_in_count == 1)
-      (1..38).each do |n|
-        h = 2
-        if n < 20
-          h = 1
-        end
-        Pick.new(user_id: current_user.id, matchday: n, half: h).save
-      end
-    end
+    
   end
   def schedule
     @matches = FootballData.fetch(:competitions,:matches, id: 2021)['matches']
