4b1c44b42696a882f78963060d4e38e44a103257
John <jjh5166@gmail.com>
2018-08-15 22:47:06 -0400

    matchday view, lock removed my picks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  
 
   FootballData.configure do |config|
     # get api key at 'http://api.football-data.org/register'
@@ -15,4 +14,23 @@ class ApplicationController < ActionController::Base
   end
   #create a pick for each matchday on first log in
 
+  private
+    def pick_timer
+      all_matches = FootballData.fetch(:competitions,:matches, id: 2021)['matches']
+      @matchtimes = {}
+      (1..38).each do |m|
+        @matchtimes[m] = []
+        all_matches.each do |t|
+          if t['matchday'] == m
+            @matchtimes[m].push(t['utcDate'])
+          end
+        end
+      end
+      @md_count = 0
+      (1..38).each do |t|
+        if Time.now.utc > @matchtimes[t].min.in_time_zone('UTC')
+          @md_count += 1
+        end
+      end
+    end
 end
