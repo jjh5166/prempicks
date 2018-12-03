72902cd01315b78c5032b2958948383bc7d16148
John <jjh5166@gmail.com>
2018-12-03 10:30:09 -0500

    Forgot Pword button, flashes.js

--- app/assets/javascripts/flashes.js
+++ app/assets/javascripts/flashes.js
@@ -1,4 +1,14 @@
-function hideFlash(){
-  $(".messages").hide();
+// function hideFlash(){
+//   $(".messages").hide();
+// }
+
+// window.addEventListener('load',
+//   function(){
+//     this.console.log('test');
+//     setTimeout(hideFlash, 3000);
+//   }, false);
+function hideFlash() {
+  setTimeout(function(){
+    $(".messages").hide();
+  }, 3000);
 }
-setTimeout(hideFlash, 3000);
