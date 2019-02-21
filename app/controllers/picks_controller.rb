0d0b1ab3c386258f6d6011d583040ba276232d00
John <jjh5166@gmail.com>
2019-02-21 18:14:58 -0500

    refactoring

--- app/controllers/picks_controller.rb
+++ app/controllers/picks_controller.rb
@@ -1,38 +1,25 @@
+# frozen_string_literal: true
+
 class PicksController < ApplicationController
   include PicksHelper
-  before_action :pick_timer, only: [:standings, :mypicks]
-  before_action :authenticate_user!, only: [:mypicks, :make]
-  before_action :team_codes_init, :pick_initialization, only: [:mypicks]
-  before_action :get_picks, only: [:standings]
-  before_action :find_md_to_lock, only: [:mypicks]
-  before_action :locked_matchdays, only: [:mypicks]
+  before_action :pick_timer, only: %i[standings mypicks]
+  before_action :authenticate_user!, only: %i[mypicks make]
+  before_action :seed_picks, :team_codes_init, :pick_initialization, only: [:mypicks]
+  before_action :load_standings, only: [:standings]
 
   def standings
-    (@md_count < 20) ? @first_timer = @md_count : @first_timer = 19
+    @first_timer = @md_count < 20 ? @md_count : 19
     @second_timer = @md_count - @first_timer
 
     if users_no_pick(@md_count).any?
       auto_pick(@md_count, @no_pick_users)
     end
   end
+
   def mypicks
-    # If first sign in and no picks exist for user, create a pick for each matchday
-    if (current_user.sign_in_count == 1)
-      if Pick.where(user_id: current_user.id).count == 0
-        (1..38).each do |n|
-          h = 2
-          if n < 20
-            h = 1
-          end
-          Pick.new(user_id: current_user.id, matchday: n, half: h).save
-        end
-      end
-    end
-
-    @matches = FootballData.fetch(:competitions,:matches, id: 2021)['matches'].sort_by { |match| [match['matchday'], match['utcDate']] }
-    # Call for matches specific team by code
-    # @matches = FootballData.fetch(:teams,:matches, id: 73)
-
+    @locked_mds = locked_mds
+    @test_times = unlocked_matchday_times
+    @matches = FootballData.fetch(:competitions, :matches, id: 2021)['matches'].sort_by { |match| [match['matchday'], match['utcDate']] }
     @user_picks_1h = []
     @user_picks_2h = []
     Pick.where(user_id: current_user.id, half: 1).each do |p|
@@ -45,18 +32,18 @@ class PicksController < ApplicationController
     @avail_teams_2h = @pickteams - @user_picks_2h
 
     @userid = current_user.id
-
   end
 
   def make
     @pick = Pick.find_by_id(params[:pick_id])
-      if @pick.update(pick_params)
-        flash[:alert] = 'Pick Saved'
-      else
-        flash[:alert] = 'Pick Unsuccesful'
-      end
+    if @pick.update(pick_params)
+      flash[:alert] = 'Pick Saved'
+    else
+      flash[:alert] = 'Pick Unsuccesful'
+    end
     redirect_back fallback_location: mypicks_path
   end
+
   private
 
   def pick_params
@@ -69,7 +56,7 @@ class PicksController < ApplicationController
     @allteams = JSON.parse(file)
     @pickteams = []
     @allteams.each do |t|
-    @pickteams.push(t['code'])
+      @pickteams.push(t['code'])
     end
   end
 end
