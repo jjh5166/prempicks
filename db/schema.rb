962b0dd1b7cb465533a50954efa24291fe575746
John <jjh5166@gmail.com>
2019-04-13 23:45:07 -0400

    added scores table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_08_231456) do
+ActiveRecord::Schema.define(version: 2019_04_12_214746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 2019_02_08_231456) do
     t.integer "half"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "scores", force: :cascade do |t|
+    t.integer "matchday", null: false
+    t.integer "points", default: 0
+    t.string "team_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["team_id", "matchday"], name: "score_key", unique: true
   end
 
   create_table "users", force: :cascade do |t|
