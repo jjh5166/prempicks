4083b22dae34b1f1b9e46d706136003fa541480a
John <jjh5166@gmail.com>
2019-04-14 00:06:09 -0400

    indexes added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_12_214746) do
+ActiveRecord::Schema.define(version: 2019_04_14_035500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,9 @@ ActiveRecord::Schema.define(version: 2019_04_12_214746) do
     t.integer "half"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id", "half"], name: "half_picks"
+    t.index ["user_id", "matchday"], name: "user_pick", unique: true
+    t.index ["user_id"], name: "index_picks_on_user_id"
   end
 
   create_table "scores", force: :cascade do |t|
