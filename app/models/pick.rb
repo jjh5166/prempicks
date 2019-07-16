ed0fb8d5d9eb57ed46742f0f18dffdb640b10bd6
John <jjh5166@gmail.com>
2019-07-15 21:31:30 -0400

    nested attributes for mypicks

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -2,7 +2,7 @@ class Pick < ApplicationRecord
   # after_initialize :set_defaults, unless: :persisted?
   # The set_defaults will only work if the object is new
   strip_attributes
-  belongs_to :user
+  belongs_to :user, inverse_of: :picks
   validates :team_id, uniqueness: { scope: %i[half user_id] }, allow_blank: true, on: :update
   # restricts user from picking same team twice in same half
   validates :user_id, uniqueness: { scope: :matchday }
