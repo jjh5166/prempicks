d4870665538f9fcc1a03fd49f7413c63419f4e08
John <jjh5166@gmail.com>
2019-07-16 20:10:31 -0400

    format

--- app/controllers/users/update_picks_controller.rb
+++ app/controllers/users/update_picks_controller.rb
@@ -2,17 +2,17 @@
 
 class Users::UpdatePicksController < Devise::RegistrationsController
 
-    def update_picks
-      @user = User.find(current_user.id)
-      if @user.update_attributes(picks_params)
-        redirect_to mypicks_path
-      else
-        # error
-        redirect_to mypicks_path
-      end
+  def update_picks
+    @user = User.find(current_user.id)
+    if @user.update_attributes(picks_params)
+      redirect_to mypicks_path
+    else
+      # error
+      redirect_to mypicks_path
     end
+  end
 private
-    def picks_params
-      params.require(:user).permit(:id, picks_attributes:[:id, :team_id, :user_id])
-    end
+  def picks_params
+    params.require(:user).permit(:id, picks_attributes:[:id, :team_id, :user_id])
+  end
 end
