aaf14a6429856093ffe7697041490d1c984eb2e7
John <jjh5166@gmail.com>
2019-07-19 15:25:41 -0400

    mdcount phase out

--- app/helpers/epldata_helper.rb
+++ app/helpers/epldata_helper.rb
@@ -4,4 +4,12 @@ module EpldataHelper
   def fetch_matches
     FootballData.fetch(:competitions, :matches, id: 2021)['matches']
   end
+  def matches_and_current
+    match_data = fetch_matches
+    @currentMatchday = match_data[0]['season']['currentMatchday']
+    return match_data
+  end
+  def set_current_matchday
+    @currentMatchday = fetch_matches[0]['season']['currentMatchday']
+  end
 end
