d773da4d70f6e8a0707ebd1007c261b7465aa347
John <jjh5166@gmail.com>
2019-07-18 21:54:55 -0400

    rm guest logout

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,10 +6,4 @@ class StaticPagesController < ApplicationController
     create_guest_user unless user_signed_in?
   end
 
-  private
-
-  def log_out_guest
-    session[:guest_user_id] = nil
-    flash.clear
-  end
 end
