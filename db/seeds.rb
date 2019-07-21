cb5ee5880e0e2b79087e86c14861cc45e1be5a8a
John <jjh5166@gmail.com>
2019-07-21 01:05:52 -0400

    seed with new helper

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 include AutopickHelper
-
+include EpldataHelper
 (1..38).each do |week|
   Matchday.create(week:week)
 end
 
-teams = lastyr_standings
+teams = last_yr_standings
 teams.each do |t|
   (1..38).each do |week|
     Score.create(team_id:t, matchday:week)
