bc445fde55e8a4e5e5118939d16db71fa698803d
John <jjh5166@gmail.com>
2019-07-22 23:47:12 -0400

    pickFrom js update

--- app/assets/javascripts/mypicks.js
+++ app/assets/javascripts/mypicks.js
@@ -1,12 +1,17 @@
 picksForm = () => {
   $(function () {
     $('.mypicks_form').change(function () {
-      let selects = $(this).find('.mp_select').serializeArray();
-      let vals = []
-      $.each(selects, function (i, v) {
-        vals.push(v['value'])
+      let half1 = $(this).find('.mp_select1').serializeArray();
+      let half2 = $(this).find('.mp_select2').serializeArray();
+      let vals1 = []
+      let vals2 = []
+      $.each(half1, function (i, v) {
+        vals1.push(v['value'])
       })
-      checkNoDupes(vals)
+      $.each(half2, function (i, v) {
+        vals2.push(v['value'])
+      })
+      checkNoDupes(vals1) && checkNoDupes(vals2)
         ? buttonOn()
         : buttonOff()
     });
