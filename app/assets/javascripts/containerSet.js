0545beddefde8d585c9fc205a2bc7df277c22603
John <jjh5166@gmail.com>
2019-07-28 16:17:48 -0400

    prevent error

--- app/assets/javascripts/containerSet.js
+++ app/assets/javascripts/containerSet.js
@@ -1,6 +1,9 @@
-// document.addEventListener("turbolinks:load", (e) => {
+document.addEventListener("turbolinks:load", (e) => {
   $(window).resize(function () {
     let footer = document.getElementsByClassName('foot')[0]
-    let height = footer.offsetHeight + 5
-    $('.page_container').css({ 'padding-bottom': height+'px'})
-});
+    if (footer.length){
+      let height = footer.offsetHeight + 5
+      $('.page_container').css({ 'padding-bottom': height+'px'})
+    }
+  });
+})
