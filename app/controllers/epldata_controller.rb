c517fecf8a4d45b1c1e3cd6cafab73c0af673770
John <jjh5166@gmail.com>
2019-07-28 01:02:52 -0400

    new schedule view

--- app/controllers/epldata_controller.rb
+++ app/controllers/epldata_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'active_support/core_ext/integer/inflections'
 class EpldataController < ApplicationController
   def table
     @epl_table = FootballData.fetch(:competitions, :standings, id: 2021)['standings'][0]['table']
@@ -8,6 +9,16 @@ class EpldataController < ApplicationController
   end
 
   def schedule
-    @md_matches = FootballData.fetch(:competitions, :matches, id: 2021, matchday: params[:matchday])['matches']
+    md_matches = FootballData.fetch(:competitions, :matches, id: 2021, matchday: params[:matchday])['matches']
+    @matchday = {}
+    @matchday.default_proc = proc { [] }
+    md_matches.each do |match|
+      dtime = DateTime.parse(match['utcDate'])
+      @matchday[dtime.strftime("%B #{ dtime.day.ordinalize }")] += [[match['utcDate'], match['status'],\
+                                     { 'home': [match['homeTeam']['id'],match['score']['fullTime']['homeTeam']],\
+                                      'away': [match['awayTeam']['id'], match['score']['fullTime']['awayTeam']] }]]
+    end
+    @teams = team_codes
   end
+
 end
