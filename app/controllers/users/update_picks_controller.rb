4ef877817fac6bcf722961424a877dc57d5c5c5d
John <jjh5166@gmail.com>
2019-08-03 22:36:55 -0400

    update guest picks

--- app/controllers/users/update_picks_controller.rb
+++ app/controllers/users/update_picks_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Users::UpdatePicksController < Devise::RegistrationsController
-
   def update_picks
     @user = User.find(current_user.id)
     if @user.update_attributes(picks_params)
@@ -11,8 +10,24 @@ class Users::UpdatePicksController < Devise::RegistrationsController
       redirect_to mypicks_path
     end
   end
-private
+
+  def update_guest_picks
+    @guest = User.find(guest_user.id)
+    if @guest.update_attributes(guest_picks_params)
+      redirect_to gpicks_path
+    else
+      # error
+      redirect_to gpicks_path
+    end
+  end
+
+  private
+
   def picks_params
-    params.require(:user).permit(:id, picks_attributes:[:id, :team_id, :user_id])
+    params.require(:user).permit(:id, picks_attributes: [:id, :team_id, :user_id])
+  end
+
+  def guest_picks_params
+    params.require(:user).permit(:id, guest_picks_attributes: [:id, :team_id, :user_id])
   end
 end
