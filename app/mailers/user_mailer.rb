909aceecfcd93dae064cd5031cc743aa875bdc19
John <jjh5166@gmail.com>
2019-08-06 19:47:26 -0400

    reminder emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,4 +10,10 @@ class UserMailer < ApplicationMailer
     @user = user
     mail(to:'theprempicks@gmail.com', subject: "#{@user.fname} #{@user.lname} just signed up")
   end
+
+  def reminder_email(user)
+    @user = user
+    @url  = 'prempicks.herokuapp.com/mypicks'
+    mail(to: @user.email, subject: "Don't Forget to Pick!")
+  end
 end
