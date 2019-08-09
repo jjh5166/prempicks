51f4cb031af1ca1f960319d79d501753a23b12aa
John <jjh5166@gmail.com>
2019-08-08 20:44:47 -0400

    fetch current matchday method

--- app/helpers/epldata_helper.rb
+++ app/helpers/epldata_helper.rb
@@ -5,6 +5,10 @@ module EpldataHelper
     FootballData.fetch(:competitions, :matches, id: 2021)['matches']
   end
 
+  def fetch_current_matchday
+    fetch_matches[0]['season']['currentMatchday']
+  end
+
   def last_yr_standings
     s3 = Aws::S3::Client.new
     file = s3.get_object(bucket: ENV['S3_BUCKET'], key: 'lastyr.json')
