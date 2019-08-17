17a2fda39b83c4517fc3b87b797c6583d94c8aba
John <jjh5166@gmail.com>
2019-08-17 00:19:24 -0400

    guest clear

--- app/workers/guest_clear.rb
+++ app/workers/guest_clear.rb
@@ -7,7 +7,7 @@ class GuestClear
   include Sidekiq::Worker
   sidekiq_options retry: false
   def perform
-    g_users = User.where(fname: 'guest', lname: '', team_name: '')
+    g_users = User.where(fname: 'guest', guest: true)
     g_users.destroy_all
   end
 end
