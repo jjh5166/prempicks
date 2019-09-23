0841d6b49cbcd24a978c83f222bb2a4a6afa58ac
John <jjh5166@gmail.com>
2019-09-23 15:50:28 -0400

    scoring queue

--- app/workers/scoring_job.rb
+++ app/workers/scoring_job.rb
@@ -1,9 +1,11 @@
-require 'sidekiq-scheduler'
+# frozen_string_literal: true
 
+require 'sidekiq-scheduler'
+# monitors API for matches finished, starts scoring jobs
 class ScoringJob
   include Sidekiq::Worker
   include ScoringHelper
-  sidekiq_options retry: false
+  sidekiq_options retry: false, queue: 'scoring'
   def perform(matchdays)
     matchdays.each do |md|
       md.to_s
