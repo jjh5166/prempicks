aef68a054c745cb8baa78357fe6e766f877f6422
John <jjh5166@gmail.com>
2019-09-23 15:50:41 -0400

    class doc

--- app/workers/set_matchday.rb
+++ app/workers/set_matchday.rb
@@ -1,11 +1,13 @@
-require 'sidekiq-scheduler'
+# frozen_string_literal: true
 
+require 'sidekiq-scheduler'
+# sets current matchday
 class SetMatchday
   include Sidekiq::Worker
   include EpldataHelper
   sidekiq_options retry: false
   def perform
     md = fetch_current_matchday
-    CurrentMatchday.find(1).update(matchday:md)
+    CurrentMatchday.find(1).update(matchday: md)
   end
 end
