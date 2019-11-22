9ab490c6067d7a15e318e83df280ec8c8dfcc9ae
John <jjh5166@gmail.com>
2019-11-22 11:27:56 -0500

    class doc

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
+# Landing Page
 class LandingController < ApplicationController
   before_action :log_out_guest, only: [:landing_page]
   def landing_page
-    @sign_up_deadline = Matchday.where(week:1)[0].lock_time
+    @sign_up_deadline = Matchday.where(week: 1)[0].lock_time
   end
 
   private
