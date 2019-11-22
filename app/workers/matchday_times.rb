ad12572eb398fd5c5166ce6fbdb85bd956659ff1
John <jjh5166@gmail.com>
2019-11-22 11:28:15 -0500

    class doc

--- app/workers/matchday_times.rb
+++ app/workers/matchday_times.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
-# updates dB for earliest match time of matchday
-require 'sidekiq-scheduler'
 
+require 'sidekiq-scheduler'
+# updates Db for earliest match time of matchday
 class MatchdayTimes
   include Sidekiq::Worker
   include EpldataHelper
-  sidekiq_options retry: false
   def perform
     update_locktimes
   end
