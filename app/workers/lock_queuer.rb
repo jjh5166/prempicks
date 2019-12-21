e910d5588727226732e1748681a3bb43545c12e8
John <jjh5166@gmail.com>
2019-12-21 14:29:01 -0500

    24hr queuer

--- app/workers/lock_queuer.rb
+++ app/workers/lock_queuer.rb
@@ -4,10 +4,9 @@ require 'sidekiq-scheduler'
 # checks for upcoming matches which trigger locking, schedules lock
 class LockQueuer
   include Sidekiq::Worker
-  include EpldataHelper
   sidekiq_options backtrace: true
   def perform
-    mds = Matchday.where(lock_time: Time.current..12.hours.from_now)
+    mds = Matchday.where(lock_time: Time.current..24.hours.from_now)
     mds.each do |md|
       MatchdayLock.perform_at(md.lock_time, md.week)
     end
